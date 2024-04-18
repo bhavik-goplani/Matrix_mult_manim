613987d3aeeacb569dc508a378f4a248eb06987d
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-04-17 22:24:27 -0500

    Added parallel distribution and multiplication animations

--- cannon.py
+++ cannon.py
@@ -3,9 +3,10 @@ from manim import *
 class CannonScene(Scene):
     def __init__ (self, **kwargs):
         super().__init__(**kwargs)
-        self.A = self.createSquareMatrix(3, "A", BLUE, 0.8)
-        self.B = self.createSquareMatrix(3, "B", GREEN, 0.8)
-        self.P = self.createSquareMatrix(3, "P", RED, 0.8)
+        self.A = self.createSquareMatrix(3, "A", BLUE, 0.8, 0.5)
+        self.B = self.createSquareMatrix(3, "B", GREEN, 0.8, 0.5)
+        self.P = self.createSquareMatrix(3, "P", RED, 0.8, 0.2)
+        self.C = self.createSquareMatrix(3, "C", ORANGE, 0.8, 0.5)
         self.title = Text("Cannon's Algorithm", color=WHITE).to_edge(UP + LEFT).scale(0.8)
         self.A_0 = None
 
@@ -15,14 +16,17 @@ class CannonScene(Scene):
         self.animateInitial()
         self.animateStep0()
 
+        self.play(FadeOut(self.title))
+        self.animateEnd()
+
         self.wait(2)
 
-    def createSquareMatrix(self, size, label, color, scale):
+    def createSquareMatrix(self, size, label, color, scale, opacity):
         matrix = VGroup()
         for x in range(size):
             row = VGroup()
             for y in range(size):
-                cell = Square(side_length=1*scale, fill_color=color, fill_opacity=0.5) 
+                cell = Square(side_length=1*scale, fill_color=color, fill_opacity=opacity, stroke_width=1*scale) 
                 cell_label = MathTex(label + "_{" + str(x) + str(y) + "}", color=WHITE).scale(cell.get_height())
                 cell.add(cell_label)
                 row.add(cell)
@@ -107,13 +111,24 @@ class CannonScene(Scene):
             self.play(Create(arrow), Write(text))
             self.wait(1)
 
-        self.B_0 = self.circularShiftUp1(B)
-        self.play(FadeOut(B), FadeOut(arrows), FadeOut(texts))
+        self.B_0 = self.circularShiftUp(B)
+        self.play(FadeOut(B), FadeOut(arrows), FadeOut(texts), FadeOut(step_0))
+        self.wait(1)
+
+        self.A_0.move_to(LEFT * 4).scale(0.6)
+        self.B_0.move_to(RIGHT * 4).scale(0.6)
+        P.move_to(ORIGIN).scale(1.2)
+
+        self.play(Create(self.A_0), Create(self.B_0), Create(P))
+        self.wait(1)
+        self.parallelDistributeMatrix(self.A_0, self.B_0, P)
+
+        self.play(FadeOut(self.A_0), FadeOut(self.B_0), FadeOut(P))
+
         pass
 
     def circularShiftLeft(self, matrix):
         # Create a new VGroup to hold the new matrix
-        scale = 0.8
         new_matrix = VGroup()
         elements = []
 
@@ -129,32 +144,13 @@ class CannonScene(Scene):
                 # Add the copied element to the row
                 elements.append(element)
                 row.add(element)
-            new_matrix.arrange(DOWN, buff=0.1*scale)
+            new_matrix.add(row)
         self.wait(1)
         
-        self.play(*[FadeOut(element) for element in elements], run_time=2)
-        return new_matrix
-        
-    def circularShiftUp(self, matrix):
-        scale = 0.8
-        new_matrix = VGroup()
-        elements = []
-
-        for i in range(3):
-            row = VGroup()
-            for j in range(3):
-                element = matrix[(j+i)%3][j].copy()
-                self.play(element.animate.move_to(matrix[i][j].get_center() + RIGHT*8), run_time=2)
-                elements.append(element)
-                row.add(element)
-            new_matrix.arrange(DOWN, buff=0.1*scale)
-        self.wait(1)
-
         self.play(*[FadeOut(element) for element in elements], run_time=2)
         return new_matrix
     
-    def circularShiftUp1(self, matrix):
-        scale = 0.8
+    def circularShiftUp(self, matrix):
         new_matrix = VGroup()
         elements = []
 
@@ -165,9 +161,81 @@ class CannonScene(Scene):
                 self.play(element.animate.move_to(matrix[j][i].get_center() + RIGHT*6), run_time=2)
                 elements.append(element)
                 col.add(element)
-            new_matrix.arrange(RIGHT, buff=0.1*scale)
+            new_matrix.add(col)
         self.wait(1)
 
         self.play(*[FadeOut(element) for element in elements], run_time=2)
         return new_matrix
 
+    def parallelDistributeMatrix(self, A, B, P):
+        # Distribute each element in A and B to P in parallel to compute the product
+        animations = [
+            ApplyMethod(A[i][j].move_to, P[i][j].get_center() + UP*0.25 + LEFT*0.25)
+            for i in range(len(A))
+            for j in range(len(A[i]))
+        ] + [
+            ApplyMethod(B[j][i].move_to, P[i][j].get_center() + UP*0.25 + RIGHT*0.25)
+            for i in range(len(B))
+            for j in range(len(B[i]))
+        ]
+
+        self.play(*animations, run_time=3)
+        self.wait(2)
+
+        self.C.scale(0.6)
+
+        animationYellow = []
+        animationMultiply = []
+        animationFadeOut = []
+        animationTransform = []
+        
+        for i in range(len(P)):
+            for j in range(len(P[i])):
+                # Highlight the elements of A and B
+                animationYellow.append(A[i][j].animate.set_color(YELLOW))
+                animationYellow.append(B[j][i].animate.set_color(YELLOW))
+
+                # Show an animation of the multiplication operation
+                multiply_symbol = MathTex(r"\times").move_to(P[i][j].get_center() + UP*0.25)
+                animationMultiply.append(Write(multiply_symbol))
+
+                self.C[i][j].move_to(P[i][j].get_center() + UP*0.25)
+
+                # Fade in the result of the multiplication operation
+                animationFadeOut.append(FadeOut(multiply_symbol))
+                animationTransform.append(Transform(A[i][j], self.C[i][j]))
+                animationTransform.append(Transform(B[j][i], self.C[i][j]))
+                
+
+        self.play(*animationYellow, run_time=1)
+        self.wait(1)
+        self.play(*animationMultiply, run_time=1)
+        self.wait(1)
+        self.play(*animationFadeOut, run_time=1)
+        self.wait(1)
+        self.play(*animationTransform, run_time=2)
+        self.wait(2)
+        pass
+
+    def animateEnd(self):
+        # Create the shape of a heart using a Polygon
+        circle_left = Circle(radius=1.5, color=RED).shift(LEFT)
+        circle_right = Circle(radius=1.5, color=RED).shift(RIGHT)
+        circle_left.set_fill(RED, opacity=1)
+        circle_right.set_fill(RED, opacity=1)
+        triangle = Polygon([-2, -1, 0], [2, -1, 0], [0, -3, 0], color=RED).set_fill(RED, opacity=1)
+
+        # Group the shapes to form the heart
+        heart = VGroup(circle_left, circle_right, triangle)
+
+        heart.move_to(ORIGIN)
+        heart.scale(0.1)
+        # Animate the heart
+        self.play(GrowFromCenter(heart))
+        self.wait(1)
+
+        # Display "Made with heart using Manim"
+        text_left = Text("Made with").next_to(heart, LEFT)
+        text_right = Text("using Manim").next_to(heart, RIGHT)
+        self.play(Write(text_left), Write(text_right))
+        pass
--- scene.py
+++ scene.py
@@ -2,6 +2,25 @@ from manim import *
 
 class CreateCircle(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
+        # Create the shape of a heart using a Polygon
+        circle_left = Circle(radius=1.5, color=RED).shift(LEFT)
+        circle_right = Circle(radius=1.5, color=RED).shift(RIGHT)
+        # Fill circle_left and circle_right with RED color
+        circle_left.set_fill(RED, opacity=1)
+        circle_right.set_fill(RED, opacity=1)
+        triangle = Polygon([-2, -1, 0], [2, -1, 0], [0, -3, 0], color=RED).set_fill(RED, opacity=1)
+
+        # Group the shapes to form the heart
+        heart = VGroup(circle_left, circle_right, triangle)
+        
+
+        heart.move_to(ORIGIN)
+        heart.scale(0.1)
+        # Animate the heart
+        self.play(GrowFromCenter(heart))
+        self.wait(1)
+
+        # Display "Made with heart using Manim"
+        text_left = Text("Made with").next_to(heart, LEFT)
+        text_right = Text("using Manim").next_to(heart, RIGHT)
+        self.play(Write(text_left), Write(text_right))
